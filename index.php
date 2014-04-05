1bab4f1f93414411b22226bfffcf60272bf9b2b6
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-05 11:33:14 +0000

    Plugin Update New Version with more options & customizations
    git-svn-id: https://plugins.svn.wordpress.org/simple-bar/trunk@887926 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -25,12 +25,31 @@ function my_popup_html(){
   $mcb_bar_height = get_option('mcb_bar_height');
   $mcb_text_wrap = get_option('mcb_text_wrap');
   $mcb_bar_position = get_option('mcb_bar_position');
-	?> 
+  $mcb_cta_button = get_option('mcb_button_cta');
+  $mcb_cta_email = get_option('mcb_email_cta');
+  $mcb_gradient_color =get_option('mcb_gradient_color');
+  $mcb_cta_email_url =get_option('mcb_cta_email_url');
+  $mcb_cta_button_url = get_option('mcb_cta_button_url');
+  $mcb_cta_button_text = get_option('mcb_cta_button_text');
+  $mcb_border_color = get_option('mcb_border_color');
+  $mcb_border_width = get_option('mcb_border_width');
+  $mcb_border_position =get_option('mcb_border_position');
+  $mcb_close_position = get_option('mcb_close_position');
+  $mcb_show_close_button = get_option('mcb_show_close_button');
+  ?> 
   <div id="mp_wrap">
-   <span id="mp_close"><b>Close</b></span>
+   <div id="mp_close"><b>Close</b></div>
    
        <div id="main_mp_box">
-       <div id="mp_content"><?php echo $mcb_content;?></div>
+
+       <div id="mp_content" style="font-family:helvetica, verdana, oswald, arial;"><b><?php echo $mcb_content;?></b><div id="cta_div" style="display:inline; text-align:center;">
+      
+        
+
+       <a href="<?php echo $mcb_cta_button_url;?>" style="text-decoration:none;"><button id="mcb_cta_button"style="display:<?php echo $mcb_cta_button;?>;margin-left:10px; background-color:black; border-radius:5px; height:28px; min-width:100px; color:#fff; border:none; text-align:center;font-size:13px;
+        "><b><?php echo $mcb_cta_button_text;?></b></button></a>
+      </div>
+    </div>
        </div>
    
       <span id="mp_open"><b>Open</b></span>
@@ -46,18 +65,17 @@ add_filter('wp_footer','my_popup_html');
 //Adding styles
 add_action( 'init', 'register_mpb_styles' );
   function register_mpb_styles() {
-  wp_register_style( 'mpb_box_style', plugins_url('Simple-Bar/mp_style.css'));
-	wp_enqueue_style( 'mpb_box_style' );
+  wp_register_style( 'mpb_box_style', plugins_url('simple-bar-premium/mp_style.css'));
+  wp_enqueue_style( 'mpb_box_style');
 }
 
 
  function my_mpb_script() {
-	wp_enqueue_script( 'mpb_box_style', plugins_url('Simple-Bar/mp_script.js'));
+  wp_enqueue_script( 'mpb_box_style', plugins_url('simple-bar-premium/mp_script.js'));
   
 }
 wp_enqueue_script("jquery");
-add_action( 'init', 'my_mpb_script' );
-
+add_action( 'init', 'my_mpb_script');
 //Adding Options
 
 register_activation_hook(__FILE__,'mcb_activating_options');
@@ -67,7 +85,17 @@ function mcb_activating_options(){
   add_option('mcb_bar_width','4%');
   add_option('mcb_text_wrap','');
   add_option('mcb_bar_position','');
-
+  add_option('mcb_button_cta');
+  add_option('mcb_email_cta');
+  add_option('mcb_gradient_color');
+  add_option('mcb_cta_email_url');
+  add_option('mcb_cta_button_url');
+  add_option('mcb_cta_button_text','Click Here');
+  add_option('mcb_border_color','#000');
+  add_option('mcb_border_width','3px');
+  add_option('mcb_border_position','top');
+  add_option('mcb_close_position','');
+  add_option('mcb_show_close_button');
 }
 
 
@@ -79,7 +107,18 @@ function mcb_sett_to_head(){
  $mcb_bar_height = get_option('mcb_bar_height');
  $mcb_text_wrap = get_option('mcb_text_wrap');
  $mcb_bar_position = get_option('mcb_bar_position');
- include 'mp_style.php';
+ $mcb_cta_button = get_option('mcb_button_cta');
+  $mcb_cta_email = get_option('mcb_email_cta');
+  $mcb_gradient_color = get_option('mcb_gradient_color');
+  $mcb_cta_email_url =get_option('mcb_cta_email_url');
+  $mcb_cta_button_url = get_option('mcb_cta_button_url');
+  $mcb_cta_button_text = get_option('mcb_cta_button_text');
+  $mcb_border_color = get_option('mcb_border_color');
+  $mcb_border_width = get_option('mcb_border_width');
+  $mcb_border_position =get_option('mcb_border_position');
+  $mcb_close_position =get_option('mcb_close_position');
+  $mcb_show_close_button =get_option('mcb_show_close_button');
+  include 'mp_style.php';
 
 }
 
@@ -90,29 +129,42 @@ function mcb_reg_sett(){
   register_setting('mcb_setting_group','mcb_bar_height');
   register_setting('mcb_setting_group','mcb_text_wrap');
   register_setting('mcb_setting_group','mcb_bar_position');
+  register_setting('mcb_setting_group','mcb_button_cta');
+  register_setting('mcb_setting_group','mcb_email_cta');
+  register_setting('mcb_setting_group','mcb_gradient_color');
+  register_setting('mcb_setting_group','mcb_cta_email_url');
+  register_setting('mcb_setting_group','mcb_cta_button_url');
+  register_setting('mcb_setting_group','mcb_cta_button_text');
+  register_setting('mcb_setting_group','mcb_border_color');
+  register_setting('mcb_setting_group','mcb_border_width');
+  register_setting('mcb_setting_group','mcb_border_position');
+  register_setting('mcb_setting_group','mcb_close_position');
+  register_setting('mcb_setting_group','mcb_show_close_button');
 }
 
 add_action('admin_menu','mpb_menu_set');
 
 function mpb_menu_set(){
   add_menu_page(
-  'Message Bar',
+  'Message Bar ',
   'Message Bar',
   'administrator',
-  'mpb_settings',
-  'admin_mpb_page' 
+  'mpb_settings_f',
+  'admin_mpb_page_f' 
 
   );
   }
-function admin_mpb_page(){
+function admin_mpb_page_f(){
   include 'mp_style.php';
    ?>
      <?php settings_fields( 'mcb_setting_group' );?>
 <?php do_settings_sections( 'mcb_setting_group' );?>
-<div class="formLayout">
-  <h1>Bar Plugin</h1>
-  <br>
-   <a href="http://notificationbar.blogspot.com/"><button style="height:90px; width:750px; font-size:25px; color:#fff; 
+  <h1>Message Bar Plugin</h1>
+
+   <br>
+    <br>
+    <div class="formLayout">
+     <a href="http://notificationbar.blogspot.com/"><button style="height:90px; width:750px; font-size:25px; color:#fff; 
     background: #a90329;
 background: -moz-linear-gradient(top,  #a90329 0%, #8f0222 44%, #6d0019 100%);
 background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#a90329), color-stop(44%,#8f0222), color-stop(100%,#6d0019));
@@ -126,64 +178,126 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', end
    <br>
    <br>
     <br>
+
+
+
    <form method="post" action="options.php" id="content_input_form">
         <?php settings_fields( 'mcb_setting_group' );?>
 <?php do_settings_sections( 'mcb_setting_group' );?>
-  <label for="mcb_bar_height"><span style="color :red;"> * </span> Bar Height :</label>
-  <input type="number" style="width:10%;" name="mcb_bar_height" value="<?php echo get_option('mcb_bar_height'); ?>" max="6"/>
+  <label for="mcb_bar_height"><span style="color :red;"> * </span> <b>Bar Height :</b></label>
+  <input type="number" style="width:10%;" name="mcb_bar_height" min="1" max="100"  value="<?php echo get_option('mcb_bar_height'); ?>"/>
   <br>
+  <label for="mcb_gradient_color"><h4>CSS Gradients : </h4></label>
+  <input type="text" style="width:200px height:300px;" name="mcb_gradient_color" value="<?php get_option('mcb_gradient_color'); ?>">
   <br>
    <label for="mcb_background_color"><b>Background Color  :</b> </label>
-   <input type="color" name="mcb_background_color" class="mcb_bg_color" value="<?php echo get_option('mcb_background_color'); ?>">
+   <input type="color" name="mcb_background_color" class="mcb_bg_color" value="<?php echo get_option('mcb_background_color'); ?>"> 
    <br>
-   
-   <textarea placeholder="Insert your css gradient code here." disabled></textarea>
    <br>
-   <h3>Position </h3>
+   <br>
    <label for="mcb_bar_position"><span style="color :red;"> * </span> Bottom : </label>
-   <input type="radio" name="mcb_bar_position" value="bottom:0%;" checked style="width:10px;">
+   <input type="radio" name="mcb_bar_position" value="bottom:0%;" checked  style="width:10px"
+
+<?php checked('bottom:0%;', get_option('mcb_bar_position')); ?>
+
+   >
    <br>
    <br>
-   <label for="mcb_bar_position" style="opacity:0.7;"><span style="color :red;">*</span> Top : </label>
-   <input type="radio"  name="mcb_bar_position" value="" disabled style="width:10px;">
+   <label for="mcb_bar_position"><span style="color :red;">*</span> Top : </label>
+   <input type="radio" name="" value="top:0%;" style="width:10px" disabled
+
+
+   >
    <br>
    <br>
    <label for="mcb_text_wrap"><b>Text Wrap : </b></label>
-   <input type="checkbox" name="mcb_text_wrap" class="mcb_text_wrap" value='1em' style="width:10px;"><p id="textwrap_msg">Select only if there are more lines than one.</p>
+   <input type="checkbox" name="mcb_text_wrap" class="mcb_text_wrap" value='1em' style="width:10px;"
+<?php checked('1em', get_option('mcb_text_wrap')); ?>
+   ></div>
+   
+   <div class="formLayout">
+   <hr style="">
+   <br>
+   <br>
 
-    <label for="mcb_button_cta">Call To Action Button :</label>
-<select name="mcb_button_cta" disabled>
-  <option value="visible">On</option>
-  <option value="none">Off</option>
+   <label for="mcb_border_position"><b>Border - Position : </b></label>
+   <select name="mcb_border_position">
+   <option value="top"  
 
+<?php selected('top', get_option('mcb_border_position')); ?>
+
+   >Top</option>
+   <option value="bottom"
+
+<?php selected('bottom', get_option('mcb_border_position')); ?>
+
+   >Bottom</option>
+   </select>
+   <br>
+   <br>
+   <label for="mcb_border_color"><b>Border Color : </b></label>
+   <input type="color" name="mcb_border_color" value="<?php echo get_option('mcb_border_color');?>">
+<label for="mcb_border_width"><b>Width : </b></label>
+   <input type="number" name="mcb_border_width" value="<?php echo get_option('mcb_border_width'); ?>" >
+   <br>
+   <hr>
+   <br>
+   <label for="mcb_show_close_button">Enable Close/Open Button:</label>
+  <select name="mcb_show_close_button">
+<option value="visible"  selected
+
+<?php selected('visible', get_option('mcb_show_close_button')); ?>
+>Enabled</option>
+<option value="visible" disabled>Disabled</option>
+  </select>
+  <br>
+  <br>
+  <label for="mcb_close_position">Close/Open Button Position : </label>
+  <select name="mcb_close_position">
+ <option value="right:0;" 
+
+<?php selected('right:0;', get_option('mcb_close_position')); ?>
+
+ >Right</option>
+ <option value="left:0;"
+
+<?php selected('left:0;', get_option('mcb_close_position')); ?>
+
+ >Left</option>
+  </select>
+  <br>
+  <br>
+  <label for="mcb_button_cta">Call To Action Button :</label>
+<select name="mcb_button_cta">
+  <option value="inline"
+<?php selected('inline', get_option('mcb_button_cta')); ?>
+  >On</option>
+  <option value="none" 
+
+<?php selected('none', get_option('mcb_button_cta')); ?>
+   >Off</option>
 </select>
+<br>
+<br>
+<label for="mcb_cta_button_url">Call To Action Button :</label>
+  <input type="url" name="mcb_cta_button_url" value="<?php echo get_option('mcb_cta_button_url');?>">
+<br>
+<br>
+<label for="mcb_cta_button_text">Button Text :</label>
+<input type="text" name="mcb_cta_button_text" value="<?php echo get_option('mcb_cta_button_text');?>">
 <br>
   <br>
   <label for="mcb_email_cta">Call To Action Email field :</label>
-  <select name="mcb_email_cta" disabled>
-  <option value="visible">On</option>
-  <option value="none"  >Off</option>
+  <select name="">
+  <option value="inline" disabled
+
+  >On</option>
+  <option value="none" disabled selected
+
+  >Off</option>
 </select>
-<br>
-<br>
-   <label for="mcb_border_color"><b>Border Color : </b></label>
-   <input type="color" name="mcb_border_color" value="<?php echo get_option('mcb_border_color');?>" disabled>
-<label for="mcb_border_width"><b>Width : </b></label>
-   <input type="number" name="mcb_border_width" value="<?php echo get_option('mcb_border_width'); ?>"  disabled>
-   <br>
-   <hr>
-<h4>Add Social Icons</h4>
-<label name="facebook"><b>Facebook :</b></label>
-<input type="checkbox" name="Facebook" disabled>
-<br>
-<label name="twitter"><b>Twitter :</b></label>
-<input type="checkbox" name="twitter" disabled>
-<br>
-<label name="twitter"><b>Pinterest :</b></label>
-<input type="checkbox" name="twitter" disabled>
-
-
-  
+  <br>
+  <br>
    <?php
    $settings = array('media_buttons'=> false,'mcb_content');
    $mcb_content = get_option('mcb_content');
@@ -191,12 +305,11 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', end
 
 
    ?>
-   <?php submit_button( 'Save Content');  ?>
-
+   <?php submit_button( 'Save Changes');  ?>
  </form>
 </div>
- 
- <div id="sp_meesage"><a href="http://sites.fastspring.com/wpscrolltotop/product/notificationbarsupport" target="_blank"><button <button style="height:110px; font-size:25px; color:#fff; 
+
+<div id="sp_meesage"><a href="http://sites.fastspring.com/wpscrolltotop/product/notificationbarsupport" target="_blank"><button <button style="height:110px; font-size:25px; color:#fff; 
     background: #a90329;
 background: -moz-linear-gradient(top,  #a90329 0%, #8f0222 44%, #6d0019 100%);
 background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#a90329), color-stop(44%,#8f0222), color-stop(100%,#6d0019));
@@ -206,7 +319,7 @@ background: -ms-linear-gradient(top,  #a90329 0%,#8f0222 44%,#6d0019 100%);
 background: linear-gradient(to bottom,  #a90329 0%,#8f0222 44%,#6d0019 100%);
 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', endColorstr='#6d0019',GradientType=0 );
   font-family:verdana; box-shadow:1px  5px 55px #000;border:none;">For Premium support and custom plugin customizations <span style="color:#3498db;font-size:30px"><b>Click Here</b></span> <p style="font-size:11px; float:left;">* Premium Version Included + Additional customizations according to your theme and requirements.</p> </button></a></div>
-  
+ 
  <style type="text/css">
  #wpfooter{
   display:none;
--- mp_style.php
+++ mp_style.php
@@ -14,12 +14,16 @@
 #main_mp_box{
 	position:fixed;
 	background-color:<?php echo $mcb_background_color; ?>;
+  <?php echo $mcb_gradient_color;?>
 	width: 100%;
 	min-width:90%; 
 	height:<?php echo $mcb_bar_height;?>%;
 	min-height:2%; 
   text-align:center;  
   <?php echo $mcb_bar_position;?>
+  border-<?php echo $mcb_border_position?>:solid <?php echo $mcb_border_width;?>px <?php echo $mcb_border_color;?>; 
+  box-shadow:2px 2px 5px #7f8c8d;
+  float: left;  
   }
 #mp_content{
   width: 90%;
@@ -27,16 +31,19 @@
   margin: auto;
   position: absolute;
   top: 0; left: 0; bottom: 0; right: 0;
-  line-height:<?php echo $mcb_text_wrap; ?>; 
+  line-height:<?php echo $mcb_text_wrap; ?>;
+  
    }
 
 
 #mp_close{
   font-size:15px;
   position:fixed;
-  left:1px;  
+  <?php echo $mcb_close_position;?>  
   <?php echo $mcb_bar_position;?>
   color:<?php echo $mcb_background_color;?>;
+  <?php echo $mcb_gradient_color;?>
+  color: <?php echo $mcb_gradient_color;?>;
   z-index:1 ;
   font-family: verdana;
   background-color: white;
@@ -46,6 +53,7 @@
   text-align: center;
   margin:0.3em;
   opacity: 0.5;
+  display: <?php echo $mcb_show_close_button;?>;
 
 }
 #mp_close:hover{
@@ -59,9 +67,12 @@
   display: none;
   font-size: 15px;
   position:fixed;
-  left:7px;
+  
+  <?php echo $mcb_close_position;?> 
   <?php echo $mcb_bar_position;?>
   color:<?php echo $mcb_background_color;?>;
+  <?php echo $mcb_gradient_color;?>
+
   z-index:10 ;
   font-family: verdana;
   background-color: white;
@@ -78,10 +89,7 @@
   cursor:pointer; 
 }
 
-#content_input_form{
-  max-width:500px;
-  max-height: 600px;
-}
+
 #textwrap_msg{
   border: 2px solid red;
   position:relative;
@@ -91,13 +99,68 @@
   visibility: hidden;
 }
 
-#content_input_form input{
-margin-left: 15px;
+
+
+#wpbody-content{
+  background: #a9db80; /* Old browsers */
+background: -moz-linear-gradient(top,  #a9db80 0%, #96c56f 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#a9db80), color-stop(100%,#96c56f)); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(top,  #a9db80 0%,#96c56f 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(top,  #a9db80 0%,#96c56f 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(top,  #a9db80 0%,#96c56f 100%); /* IE10+ */
+background: linear-gradient(to bottom,  #a9db80 0%,#96c56f 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a9db80', endColorstr='#96c56f',GradientType=0 ); /* IE6-9 */
+
 }
 
 
 
 
+.formLayout
+    {
+
+        
+        padding: 10px;
+        width: 450px;
+        margin: 10px;
 
 
+
+
+        
+
+    }
+    
+    .formLayout label 
+    {
+        display: block;
+        width: 195px;
+        float: left;
+        margin-bottom: 20px;
+        margin-left: 20px;
+    }
+    .formLayout input{
+          display: block;
+        width: 100px;
+        float: left;
+        margin-bottom: 20px;
+
+    }
+ 
+    .formLayout label
+    {
+        text-align: right;
+        padding-right: 20px;
+        font-size: 16px;
+        font-weight: bold;
+    }
+ 
+    br
+    {
+        clear: left;
+    }
+
+
+
+    
 </style>
